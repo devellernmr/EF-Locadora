cf178f74cd58cb76aea0be9f83da4f6e031f9cd2
devellernmr <devellernmr@gmail.com>
2025-06-16 15:29:52 -0300

    feat: adicionado Hero, imagens, códigos e svg's.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import "./App.css";
+import Home from "./Components/Home/Home.jsx";
 import Navbar from "./Components/Navbar/Navbar.jsx";
 
 function App() {
   return <>
   <Navbar />
+  <Home />
   </>;
 }
 
--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,9 +1,48 @@
-function Home () {
+import "./home.css";
+import back from "/assets/img/background.png";
+import backmobile from "/assets/img/background-mobile.png";
+import what from "/assets/svg/icons8-whatsapp.svg";
+
+function Home() {
   return (
-    <div>
-      <h1>Welcome to the Home Page</h1>
-      <p>This is the home page of our application.</p>
-    </div>
+    <section className="hero">
+      <div className="hero-section">
+        <div className="content">
+          <div className="hero-background">
+            <picture>
+              {/* fonte para mobile até 425px */}
+              <source
+                media="(max-width: 425px)"
+                srcSet={backmobile}
+              />
+              {/* fallback para desktop */}
+              <img src={back} alt="Fundo do hero" />
+            </picture>
+          </div>
+          <div className="hero-content-txt">
+            <div>
+              <h1>
+                <span>COMPROMISSO</span> <br /> COM O <br />
+                <span>SUCESSO DA</span> <br /> SUA OBRA!
+              </h1>
+              <p>
+                Agilidade, confiança e os melhores equipamentos para sua
+                construção.
+              </p>
+              <div className="hero-buttons">
+                <a href="#services" className="btn btn-primary">
+                  <img src={what} alt="WhatsApp" /> Contate-nos Agora!
+                </a>
+                <a href="#contact" className="btn btn-secondary">
+                  Veja nossos produtos &#x21E9;
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="diagonal"></div>
+        </div>
+      </div>
+    </section>
   );
 }
 
